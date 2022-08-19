fe37bd92628b878c0f8e2776030039e3a3a5e4bf
Jeff Glass <glass.jeffrey@gmail.com>
2022-08-19 14:21:41 -0500

    Remove spurious import

--- test_asyncfinder.py
+++ test_asyncfinder.py
@@ -1,13 +1,15 @@
-from json import load
 from asyncfinder import is_async_visit
 import pytest
 
 EXPECTED_RESULTS = [
     ("async_finder_tests/helloworld.py", False),
-    ("async_finder_tests/await_in_asyncdef", False),
+    ("async_finder_tests/await_in_asyncdef.py", False),
+
+
     ("async_finder_tests/naked_await.py", True),
     ("async_finder_tests/naked_asyncWith.py", True),
     ("async_finder_tests/naked_asyncFor.py", True),
+
     ("async_finder_tests/importasyncio.py", False),
     ("async_finder_tests/asyncio_comment.py", False),
 ]
